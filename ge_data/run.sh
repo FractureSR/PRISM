29aa505078fa0716f751341a8d94c5458d1064f3
xuliang_uw <xuliang_uw@gpu33.cluster.local>
2026-03-26 14:36:47 +0800

    splitable run.sh

--- ge_data/run.sh
+++ ge_data/run.sh
@@ -8,36 +8,60 @@
 
 SCRIPT=ge_data_all_llama2chat.py
 
-OUTPUT_DIR=llama2-70b
-
 DATA_DIR=/mnt/inaisfs/data/home/liuf_criait/data
 DATASET_DIR=${DATA_DIR}/dataset
 MODEL_DIR=${DATA_DIR}/model/Llama-2-70b-chat-hf
 
+# Output directories (each dataset has its own)
+SHAREGPT_OUTDIR=llama2-70b/ShareGPT
+ULTRACHAT_OUTDIR_PART1=llama2-70b/UltraChat_part1
+ULTRACHAT_OUTDIR_PART2=llama2-70b/UltraChat_part2
+OPENTHOUGHTS_OUTDIR=llama2-70b/OpenThoughts2
+
+# UltraChat split configuration (adjust ULTRACHAT_SPLIT to change the ratio)
+ULTRACHAT_TOTAL=463000
+ULTRACHAT_SPLIT=231500
+
 echo "start time: $(date)"
 
+# ShareGPT
 python allocation.py \
     --script ${SCRIPT} \
-    --outdir ${OUTPUT_DIR} \
+    --outdir ${SHAREGPT_OUTDIR} \
     --data_path ${DATASET_DIR}/ShareGPT_V4.3_unfiltered_cleaned_split.json \
     --model_path ${MODEL_DIR} \
     --dataset_name ShareGPT \
-    --end_rows 68000
+    --start_row 0 \
+    --end_row 68000
 
+# UltraChat - part 1
 python allocation.py \
     --script ${SCRIPT} \
-    --outdir ${OUTPUT_DIR} \
+    --outdir ${ULTRACHAT_OUTDIR_PART1} \
     --data_path ${DATASET_DIR}/ultrachat_200k \
     --model_path ${MODEL_DIR} \
     --dataset_name UltraChat \
-    --end_rows 463000
+    --start_row 0 \
+    --end_row ${ULTRACHAT_SPLIT}
 
+# UltraChat - part 2
 python allocation.py \
     --script ${SCRIPT} \
-    --outdir ${OUTPUT_DIR} \
+    --outdir ${ULTRACHAT_OUTDIR_PART2} \
+    --data_path ${DATASET_DIR}/ultrachat_200k \
+    --model_path ${MODEL_DIR} \
+    --dataset_name UltraChat \
+    --start_row ${ULTRACHAT_SPLIT} \
+    --end_row ${ULTRACHAT_TOTAL}
+
+# OpenThoughts2
+python allocation.py \
+    --script ${SCRIPT} \
+    --outdir ${OPENTHOUGHTS_OUTDIR} \
     --data_path ${DATASET_DIR}/OpenThoughts2-1M \
     --model_path ${MODEL_DIR} \
     --dataset_name OpenThoughts2 \
-    --end_rows 269000
+    --start_row 0 \
+    --end_row 269000
 
 echo "end time: $(date)"
